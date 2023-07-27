8bced9e3fb72737297a5c44d6a64e911a22c937f
Utkarsh Singh <83408377+utkarsh7370@users.noreply.github.com>
2023-07-27 09:24:50 +0530

    Chat bot, Email js

--- LandingPage/script.js
+++ LandingPage/script.js
@@ -1,21 +1,33 @@
-let menu = document.querySelector('#menu-bar');
-let navbar = document.querySelector('.navbar');
+let menu = document.querySelector("#menu-bar");
+let navbar = document.querySelector(".navbar");
 
 menu.onclick = () => {
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-}
-
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
 
 window.onscroll = () => {
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
 
-
-    if (window.scrollY > 60) {
-        document.querySelector('#scroll-top').classList.add('active');
-    }
-    else {
-        document.querySelector('#scroll-top').classList.remove('active');
-    }
+  if (window.scrollY > 60) {
+    document.querySelector("#scroll-top").classList.add("active");
+  } else {
+    document.querySelector("#scroll-top").classList.remove("active");
+  }
+};
+function sendMail() {
+  var params = {
+    user_name: document.getElementById("fullName").value,
+    user_email: document.getElementById("email_id").value,
+    user_phone: document.getElementById("numBer").value,
+    food_name: document.getElementById("foodname").value,
+    message: document.getElementById("message").value,
+  };
+  emailjs
+    .send("service_166gbmn", "template_zbv571w", params)
+    .then(function (res) {
+      alert("Success! " + res.status);
+      //   console.log("sent");
+    });
 }
